ef9a3f0e4454257f352c1290acf5d2ef51975d09
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-22 18:51:58 -0500

    Corrected mispelling on templates/application.hbs

--- app/courses/route.js
+++ app/courses/route.js
@@ -3,17 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     // Uncomment this when the API is ready
-    // return this.store.findAll('courses');
+    return this.store.findAll('course');
 
-    return [
-      {
-        id: 1,
-        name: 'Front-Engineering'
-      },
-      {
-        id: 2,
-        name: '.NET'
-      },
-    ];
-  }
+  //   return [
+  //     {
+  //       id: 1,
+  //       name: 'Front-Engineering'
+  //     },
+  //     {
+  //       id: 2,
+  //       name: '.NET'
+  //     },
+  //   ];
+}
 });

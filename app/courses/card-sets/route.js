fb6cdbe05198b8e0625e8519ccbfe7c2531a74c1
Alia Borroho <aliaborroho@gmail.com>
2015-08-22 16:25:46 -0500

    another one

--- app/courses/card-sets/route.js
+++ app/courses/card-sets/route.js
@@ -4,7 +4,17 @@ export default Ember.Route.extend({
   model: function() {
     return {
       id: 1,
-      name: 'The Flashyard (wizards only)'
+      name: 'The Flashyard (wizards only)',
+      cardSets: [
+        {
+          id: 2,
+          name: 'Console'
+        },
+        {
+          id: 3,
+          name: 'HTML'
+        }
+      ]
     };
   },
 });

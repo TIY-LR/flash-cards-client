8b9462dc242b1d32c3c4ce943a7fe5a2acf626aa
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-22 14:20:37 -0500

    Updated route/template for card-sets

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('courses', function() {
-    this.route('card-sets', {path: '/:courseid/card-sets'},  function() {
+    this.route('card-sets', {path: '/:course_id/card-sets'},  function() {
       this.route('cards', {path: '/:card_setid/cards'}, function() {
         this.route('index', {path: '/'});
         this.route('create', {path: '/new'});

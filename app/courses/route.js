1260625aadff4f78737c9265c427684c1d11337d
Alia Borroho <aliaborroho@gmail.com>
2015-08-22 10:44:10 -0500

    merged with team on Master

--- app/courses/route.js
+++ app/courses/route.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return [
+      {
+        id: 1,
+        name: 'Front End'
+      },
+      {
+        id: 2,
+        name: '.NET'
+      },
+    ];
+  }
 });

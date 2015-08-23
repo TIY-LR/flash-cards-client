5f5d469453943a93f67d14fce52716abfaf96582
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-22 23:12:42 -0500

    Updated card create template

--- app/courses/card-sets/cards/create/route.js
+++ app/courses/card-sets/cards/create/route.js
@@ -2,8 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.createRecord('card');
+    return this.store.createRecord('card', {
+      cardSet: this.modelFor('courses.card-sets.cards')
+    });
   },
+
+  deleteUnsavedModel: function() {
+    var model = this.controller.get('model');
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  }.on('deactivate'),
 
   actions: {
     save: function(card, data) {
--- app/courses/create/route.js
+++ app/courses/create/route.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function() {
+  model: function() {
     return this.store.createRecord('course');
   },
+
+  deleteUnsavedModel: function() {
+    var model = this.controller.get('model');
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  }.on('deactivate'),
 
   actions: {
     save: function(course, data) {

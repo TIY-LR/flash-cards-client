eff6c60d938f94755ef0302dc5b93f15a9503556
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-22 21:06:58 -0500

    Created card-set create form

---
+++ app/courses/card-sets/create/route.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model: function() {
+    return this.store.createRecord('card-set');
+  },
+
+  actions: {
+    save: function(cardset, data) {
+      cardset.setProperties(data);
+
+      cardset.save().then(() => {
+        this.transitionTo('courses');
+      });
+    }
+
+  },
+});
--- app/models/card-set.js
+++ app/models/card-set.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
   cards: DS.hasMany('card'),
-  course: DS.belongsTo('course'),
+  course: DS.belongsTo('course', {async: true}),
 });

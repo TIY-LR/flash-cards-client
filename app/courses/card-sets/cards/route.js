6853ad8ae1b04b9d0d8f585548eda18c2b787ad8
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-23 14:05:33 -0500

    Applied grid row/column for card view

--- app/courses/card-sets/cards/route.js
+++ app/courses/card-sets/cards/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    actions: {
+    deleteCard: function(card) {
+      if (window.confirm('Are you sure you want to delete?')) {
+        card.destroyRecord();
+    }
+  }
+}
 });
